e6c9da218cdd5a15a9a0f60441621da158e788a7
Devloner <tminecraftben@gmail.com>
2025-12-07 20:14:16 +0200

    init

---
+++ labs_lntu_web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using labs_lntu_web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace labs_lntu_web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ labs_lntu_web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace labs_lntu_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
